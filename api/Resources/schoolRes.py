d7be578e5a29559a721d1ca7e8f42186a1c78f5f
AdiJoost <adi.joost@gmail.com>
2023-04-23 15:23:25 +0200

    post, del added

--- api/Resources/schoolRes.py
+++ api/Resources/schoolRes.py
@@ -4,9 +4,10 @@ created: April 2022
 Notes: -
 """
 
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 from Models.schoolModel import SchoolModel
-from pseudocode import create_response
+from pseudocode import create_response, getDelParser
+import os
 
 class SchoolRes(Resource):
 
@@ -16,3 +17,53 @@ class SchoolRes(Resource):
         if school:
             return create_response(school.to_json(), 200)
         return create_response({"message":"School not found"}, 404)
+    
+    def post(self, _id):
+      parser = getPoParser()
+      
+      #Gets Payload Data
+      data = parser.parse_args()
+
+      #If Payload does not contain password,
+      #reject request
+      if data["pw"] != os.environ["DBLP_API_PW"]:
+          return create_response({"message": "Unauthorized"}, 401)
+      del data["pw"]
+
+      #Create new object and update relationships
+      school = SchoolModel(**data)
+
+      #save object to db 
+      school.save()
+      return create_response(school.to_json(), 201)
+    
+    def delete(self, _id):
+        parser = getDelParser()
+        data = parser.parse_args()
+        if data["pw"] != os.environ["DBLP_API_PW"]:
+          return create_response({"message": "Unauthorized"}, 401)
+        del data["pw"]
+
+        school = SchoolModel.get(_id)
+        if not school:
+            return create_response({
+                "message": f"No school with ID={_id} found"
+            }, 200)
+        school.delete()
+        return create_response({
+            "message": f"School with ID={_id} deleted"
+        }, 200)
+
+
+
+def getPoParser():
+    #returns a reqparser for the article post method
+    parser = reqparse.RequestParser()
+    parser.add_argument("name",
+                        type=str,
+                        required=True,
+                        help="This field cannot be left blank")
+    parser.add_argument("pw",
+                        type=str,
+                        )
+    return parser
