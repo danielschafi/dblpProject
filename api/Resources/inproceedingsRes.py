3fca2f5149ca269b8ec1b0b53b3711e183397634
AdiJoost <adi.joost@gmail.com>
2023-04-23 14:24:26 +0200

    post, del added

--- api/Resources/inproceedingsRes.py
+++ api/Resources/inproceedingsRes.py
@@ -4,9 +4,10 @@ created: April 2022
 Notes: -
 """
 
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 from Models.inproceedingsModel import InproceedingsModel
-from pseudocode import create_response
+from pseudocode import create_response, getDelParser
+import os
 
 class InproceedingsRes(Resource):
 
@@ -16,3 +17,74 @@ class InproceedingsRes(Resource):
         if inproceedings:
             return create_response(inproceedings.to_json(), 200)
         return create_response({"message":"Inproceedings not found"}, 404)
+    
+    def post(self, _id):
+      parser = getPoParser()
+      
+      #Gets Payload Data
+      data = parser.parse_args()
+
+      #If Payload does not contain password,
+      #reject request
+      if data["pw"] != os.environ["DBLP_API_PW"]:
+          return create_response({"message": "Unauthorized"}, 401)
+      del data["pw"]
+
+      #Create new object and update relationships
+      inproceedings = InproceedingsModel(**data)
+
+      #save object to db 
+      inproceedings.save()
+      return create_response(inproceedings.to_json(), 201)
+    
+    def delete(self, _id):
+        parser = getDelParser()
+        data = parser.parse_args()
+        if data["pw"] != os.environ["DBLP_API_PW"]:
+          return create_response({"message": "Unauthorized"}, 401)
+        del data["pw"]
+
+        inproeedings = InproceedingsModel.get(_id)
+        if not inproeedings:
+            return create_response({
+                "message": f"No Inproceedings with ID={_id} found"
+            }, 200)
+        inproeedings.delete()
+        return create_response({
+            "message": f"Inproceedings with ID={_id} deleted"
+        }, 200)
+
+
+
+def getPoParser():
+    #returns a reqparser for the article post method
+    parser = reqparse.RequestParser()
+    parser.add_argument("title",
+                        type=str,
+                        required=True,
+                        help="This field cannot be left blank")
+    parser.add_argument("year",
+                        type=str,
+                        required=True,
+                        help="This field cannot be left blank")
+    parser.add_argument("crossref",
+                        type=str,
+                        required=True,
+                        help="This field cannot be left blank")
+    parser.add_argument("booktitle",
+                        type=str,
+                        required=True,
+                        help="This field cannot be left blank")
+    parser.add_argument("url",
+                        type=str,
+                        required=True,
+                        help="This field cannot be left blank")
+    parser.add_argument("pages",
+                        type=str,
+                        required=True,
+                        help="This field cannot be left blank")
+
+    parser.add_argument("pw",
+                        type=str,
+                        )
+    return parser
