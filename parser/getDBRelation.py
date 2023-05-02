b212a26f8b6c229cab945b05b92547ebb20fc07e
chandrsangee <sangeeths.chandrakumar@fhgr.stud.ch>
2023-05-02 22:20:00 +0200

    getDbRelation parser

--- parser/getDBRelation.py
+++ parser/getDBRelation.py
@@ -5,7 +5,7 @@ url = "127.0.0.1:5000/api"
 headers = {'Content-type': 'application/json'}
 
 
-
+#TODO: create get methods for all endpoints
 def getJournalID(journal):
     # TODO endpoint doesen't exist yet to get by journalName /journal/(str)
     response = requests.get(f"{url}/journal/{journal[0]}")
@@ -14,11 +14,7 @@ def getJournalID(journal):
         return data['name']
     else:
         return None
-
-def createJournal(journal):
-    response = requests.post(url=url+"/journal/1/", data=journal, headers=headers)
-    return response.status_code
-        
+         
 def getAuthorID(author):
     response = requests.get(f"{url}/author/{author}")
     if response.status_code == 200:
@@ -26,9 +22,83 @@ def getAuthorID(author):
         return data['name']
     else:
         return None
+    
+def getPublisherID(publisher):
+    response = requests.get(f"{url}/publisher/{publisher}")
+    if response.status_code == 200:
+        data = json.loads(response.text)
+        return data['name']
+    else:
+        return None
+    
+def getCiteID(cite):
+    response = requests.get(f"{url}/cite/{cite}")
+    if response.status_code == 200:
+        data = json.loads(response.text)
+        return data['name']
+    else:
+        return None
+    
+def getEeID(ee):
+    response = requests.get(f"{url}/ee/{ee}")
+    if response.status_code == 200:
+        data = json.loads(response.text)
+        return data['name']
+    else:
+        return None
+
+def getEeType(ee):
+    response = requests.get(f"{url}/ee/{ee}")
+    if response.status_code == 200:
+        data = json.loads(response.text)
+        return data['name']
+    else:
+        return None
+
+def getSchoolID(school):
+    response = requests.get(f"{url}/school/{school}")
+    if response.status_code == 200:
+        data = json.loads(response.text)
+        return data['name']
+    else:
+        return None
+    
+def getEditorID(editor):
+    response = requests.get(f"{url}/editor/{editor}")
+    if response.status_code == 200:
+        data = json.loads(response.text)
+        return data['name']
+    else:
+        return None
+
+
+# TODO: create post methods for all endpoints
+def createJournal(journal):
+    response = requests.post(url=url+"/journal/1/", data=journal, headers=headers)
+    return response.status_code
+
+def createPublisher(publisher):
+    response = requests.post(url=url+"/publisher/1/", data=publisher, headers=headers)
+    return response.status_code
 
 def createAuthor(author):
     response = requests.post(url=url+"/author/1/", data=author, headers=headers)
+    return response.status_code
+
+def createCite(cite):
+    response = requests.post(url=url+"/cite/1/", data=cite, headers=headers)
+    return response.status_code
+
+def createEe(ee):
+    response = requests.post(url=url+"/ee/1/", data=ee, headers=headers)
+    return response.status_code
+
+def createSchool(school):
+    response = requests.post(url=url+"/school/1/", data=school, headers=headers)
+    return response.status_code
+
+def createEditor(editor):
+    response = requests.post(url=url+"/editor/1/", data=editor, headers=headers)
     return response.status_code
 
 def createArticle(article):
--- parser/parseElement.py
+++ parser/parseElement.py
@@ -12,9 +12,6 @@ return: dict of the element
 -> Add dict to list 
 """
 
-
-
-
 def parseArticle(element):
     journalID = parseJournal(element.find("journal"))
     authors = parseAuthors(element.findall("author").text)
@@ -43,7 +40,6 @@ def parseJournal(element):
         journalID = getJournalID(element)   
     return journalID
 
-
 def parseAuthors(authors):
     authorIDList = []
     for a in authors:
@@ -53,4 +49,56 @@ def parseAuthors(authors):
             authorIDList.append(getAuthorID(a))
     return authorIDList or None
 
+def parsePublisher(publishers):
+    publisherIDList = []
+    for p in publishers:
+        publisherID = getPublisherID(p)
+        if publisherID is None:
+            createPublisher(json.dumps(p))
+            publisherIDList.append(getPublisherID(p))
+    return publisherIDList or None
 
+def parseCites(cites):
+    citesIDList = []
+    for c in cites:
+        citesID = getCitesID(c)
+        if citesID is None:
+            createCites(json.dumps(c))
+            citesIDList.append(getCitesID(c))
+    return citesIDList or None
+
+def parseEe(ees):
+    eeIDList = []
+    for e in ees:
+        eeID = getEeID(e)
+        if eeID is None:
+            createEe(json.dumps(e))
+            eeIDList.append(getEeID(e))
+    return eeIDList or None
+
+def parseEeType(eeTypes):
+    eeTypeIDList = []
+    for e in eeTypes:
+        eeTypeID = getEeTypeID(e)
+        if eeTypeID is None:
+            createEeType(json.dumps(e))
+            eeTypeIDList.append(getEeTypeID(e))
+    return eeTypeIDList or None
+
+def parseSchool(schools):
+    schoolIDList = []
+    for s in schools:
+        schoolID = getSchoolID(s)
+        if schoolID is None:
+            createSchool(json.dumps(s))
+            schoolIDList.append(getSchoolID(s))
+    return schoolIDList or None
+
+def parseEditor(editors):
+    editorIDList = []
+    for e in editors:
+        editorID = getEditorID(e)
+        if editorID is None:
+            createEditor(json.dumps(e))
+            editorIDList.append(getEditorID(e))
+    return editorIDList or None
