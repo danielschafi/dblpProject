7a96923294198bfcd684b42db75ac577b3c289a7
Daniel Schafhäutle <daniel.schafi@bluewin.ch>
2023-05-05 21:38:05 +0200

    sike blegh

--- parser/getDBRelation.py
+++ parser/getDBRelation.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-url = "127.0.0.1:5000/api"
+url = "http://127.0.0.1:5000/api"
 headers = {'Content-type': 'application/json'}
 
 
@@ -72,9 +72,8 @@ def getEditorID(editor):
         return None
 
 
-# TODO: create post methods for all endpoints
 def createJournal(journal):
-    response = requests.post(url=url+"/journal/1/", data=journal, headers=headers)
+    response = requests.post(url=url, data=journal, headers=headers)
     return response.status_code
 
 def createPublisher(publisher):
