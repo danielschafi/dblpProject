1db5d45fee0ea5f0e68020bcd437f9000441e112
chandrsangee <sangeeths.chandrakumar@fhgr.stud.ch>
2023-05-05 20:25:25 +0200

    pre programming of parser

--- parser/xmlParserBatch.py
+++ parser/xmlParserBatch.py
@@ -20,7 +20,30 @@ for event, element in etree.iterparse("testData.xml", dtd_validation=True):
     if event == "end":
         if element.tag == "article":
             par.parseArticle(element)
-            
+
+        elif element.tag == "inproceedings":
+            par.parseInproceedings(element)
+
+        elif element.tag == "proceedings":
+            par.parseProceedings(element)
+
+        elif element.tag == "book":
+            par.parseBook(element)
+
+        elif element.tag == "incollection":
+            par.parseIncollection(element)
+
+        elif element.tag == "phdthesis":
+            par.parsePhdthesis(element)
+
+        elif element.tag == "mastersthesis":
+            par.parseMastersthesis(element)
+
+        elif element.tag == "www":
+            par.parseWww(element)
+
+        elif element.tag == "data":
+            par.parseData(element)
             
             """
                 "ee" : element.find("ee").text,
