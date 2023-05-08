94af866abdf118f37da372cb379341e8ab66bbf1
chandrsangee <sangeeths.chandrakumar@fhgr.stud.ch>
2023-05-09 00:49:41 +0200

    parser

--- parser/xmlParserBatch.py
+++ parser/xmlParserBatch.py
@@ -1,20 +1,20 @@
 import lxml.etree as etree
+import os
 import json
 from getDBRelation import *
 import parseElement as par
 import requests
 from numba import jit
 import multiprocessing as mp
-import tqdm
+from tqdm import tqdm
 
 
 url = "http://127.0.0.1:5000/api"
 
+tags = ["article", "inproceedings", "proceedings", "book", "incollection", "phdthesis", "mastersthesis", "www", "data"]
 
-def parseElement(args):
-    config, i = args
-    element = config.elements[i]
-    element = etree.fromstring(element)
+
+def parseElement(element):
     if element.tag == "article":
         par.parseArticle(element)
     elif element.tag == "inproceedings":
@@ -34,23 +34,47 @@ def parseElement(args):
     elif element.tag == "data":
         par.parseData(element)
 
-def parseXML(config):
-    counter = 0
-    pool = mp.Pool()
-    tasks = []
-    elements= []
-    #do a loading bar with tqdm
-    for event, element in etree.iterparse("testData.xml", dtd_validation=True):
-        if event == "end":
-            tasks.append((config, counter))
-            elements.append(etree.tostring(element, encoding='utf8', method='xml'))
-            counter += 1
-    config.elements = elements
-    for _ in tqdm.tqdm(pool.imap_unordered(parseElement, tasks), total=len(tasks)):
-        pass
+def process_chunk(chunk):
+    for element in chunk:
+        parseElement(element)
+
+def parse_xml_chunkwise(xml_file_path, chunk_size):
+    context = etree.iterparse(xml_file_path, events=("start", "end"), dtd_validation=True, load_dtd=True)
+    _, root = next(context)  # get root element
+
+    def map_chunk_to_processes(mapped_chunk):
+        process_chunk(mapped_chunk)
+
+    chunk = []
+    for event, elem in context:
+        if event == "end" and elem.tag in tags:
+            chunk.append(elem)
+
+            if len(chunk) >= chunk_size:
+                map_chunk_to_processes(chunk)
+                root.clear()
+                chunk = []
+
+    if chunk:
+            map_chunk_to_processes(chunk)
+
+def parallel_parse_xml_chunkwise(suffix):
+    parse_xml_chunkwise("xml_chunks/" + "{:04d}".format(suffix) + "/dblp.xml", 10000)
+    print("part %d Done" % suffix)
+
+def parseXML():
+    # get number of xml files in xml_chunks folder
+    num_xml_files = len(os.listdir("xml_chunks"))
+    cores = 10
+
+    #with mp.Pool(cores) as pool:
+    #    for _ in tqdm(pool.imap_unordered(parallel_parse_xml_chunkwise, range(num_xml_files)), total=num_xml_files):
+    #        pass
+
+    ###TEST DATA
+    parse_xml_chunkwise("testData.xml", 10000)
 
 if __name__ == "__main__":
-    manager = mp.Manager()
-    config = manager.Namespace()
-    config.elements = []
-    parseXML(config)
+    parseXML()
+
+#run python xml_split.py -o xml_chunks -M 1024 dblp.xml
