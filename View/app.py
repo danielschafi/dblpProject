5786564fe6974534e3719afd6601f1d269c0bcea
chandrsangee <sangeeths.chandrakumar@fhgr.stud.ch>
2023-05-15 01:15:15 +0200

    try some styles

--- View/app.py
+++ View/app.py
@@ -1,10 +1,19 @@
 import dash
 import dash_bootstrap_components as dbc
 from dash import Input, Output, dcc, html
+import plotly.graph_objects as go
+import networkx as nx
+import requests
 
 app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-# the style arguments for the sidebar. We use position:fixed and a fixed width
+def get_api_data():
+    url = 'https://api.example.com/data'
+    response = requests.get(url)
+    data = response.json()
+    return data
+
+
 SIDEBAR_STYLE = {
     "position": "fixed",
     "top": 0,
@@ -15,8 +24,7 @@ SIDEBAR_STYLE = {
     "background-color": "#f8f9fa",
 }
 
-# the styles for the main content position it to the right of the sidebar and
-# add some padding.
+
 CONTENT_STYLE = {
     "margin-left": "18rem",
     "margin-right": "2rem",
@@ -25,10 +33,10 @@ CONTENT_STYLE = {
 
 sidebar = html.Div(
     [
-        html.H2("Sidebar", className="display-4"),
+        html.Img(src="/assets/DBLP_Logo.png", height="80px"),
         html.Hr(),
         html.P(
-            "A simple sidebar layout with navigation links", className="lead"
+            "Welcome to our Dashboard DBLP ", className="lead"
         ),
         dbc.Nav(
             [
@@ -43,7 +51,55 @@ sidebar = html.Div(
     style=SIDEBAR_STYLE,
 )
 
-content = html.Div(id="page-content", style=CONTENT_STYLE)
+
+G = nx.DiGraph()
+G.add_edges_from([(0, 1), (1, 2), (2, 3), (3, 1)])
+pos = nx.spring_layout(G)
+
+
+fig = go.Figure()
+
+
+node_x = []
+node_y = []
+node_text = []
+for node in G.nodes:
+    x, y = pos[node]
+    node_x.append(x)
+    node_y.append(y)
+    node_text.append(str(node))
+fig.add_trace(go.Scatter(
+    x=node_x,
+    y=node_y,
+    mode='markers',
+    marker=dict(size=30, color='blue'),
+    text=node_text,
+    hoverinfo='text'
+))
+
+edge_x = []
+edge_y = []
+for edge in G.edges:
+    x0, y0 = pos[edge[0]]
+    x1, y1 = pos[edge[1]]
+    edge_x.append(x0)
+    edge_x.append(x1)
+    edge_x.append(None)
+    edge_y.append(y0)
+    edge_y.append(y1)
+    edge_y.append(None)
+fig.add_trace(go.Scatter(
+    x=edge_x,
+    y=edge_y,
+    mode='lines',
+    line=dict(color='black', width=1)
+))
+
+content = html.Div(id="page-content", style=CONTENT_STYLE, children=[
+    html.H1("Home Page", className="display-4"),
+    html.Hr(),
+    dcc.Graph(figure=fig),
+])
 
 app.layout = html.Div([dcc.Location(id="url"), sidebar, content])
 
@@ -51,7 +107,7 @@ app.layout = html.Div([dcc.Location(id="url"), sidebar, content])
 @app.callback(Output("page-content", "children"), [Input("url", "pathname")])
 def render_page_content(pathname):
     if pathname == "/":
-        return html.P("This is the content of the home page!")
+        return content
     elif pathname == "/page-1":
         return html.P("This is the content of page 1. Yay!")
     elif pathname == "/page-2":
