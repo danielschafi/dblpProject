d63a884fece3d0754704b1ca58382cc5a0237668
Daniel Schafhäutle <daniel.schafi@bluewin.ch>
2023-05-20 19:44:12 +0200

    usability, bugfix

--- generateNetworkData.py
+++ generateNetworkData.py
@@ -1,9 +1,10 @@
 import random as rd
 
 
-max_dist = 5
+max_dist = 4
 filepath = "sample_networkGraphData.csv"
 
+max_nodes_per_node = 5
 
 
 data = ""
@@ -16,12 +17,13 @@ for n in range(1, max_dist + 1):
     
     # for each existing node add 0 to 5 new nodes
     # store new nodes in list
-    
+
     newNodes = [] 
 
     for node in prev_nodes:
+        newNodeCount = rd.randint(1,max_nodes_per_node) if n == 1 else rd.randint(0,max_nodes_per_node)
         
-        for _ in range(rd.randint(0,5)):
+        for _ in range(newNodeCount):
             line = f"Id_{count}, Id_{rd.choice(prev_nodes)}, {n},"
             data += line + "\n"
             newNodes.append(count)
