17777bdde550b9b8495791f3a7e7bc119e7a7a34
AdiJoost <adi.joost@gmail.com>
2023-05-25 07:29:50 +0200

    added search_order table

---
+++ api/Models/searchOrderModel.py
@@ -0,0 +1,39 @@
+"""
+Author: Adrian Joost
+created: Mai 2022
+Notes: -
+"""
+
+from db import db
+
+class SearchOrder(db.Model):
+
+    __tablename__ = "search_order"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+
+    def __init__(self, name):
+        self.name = name
+
+    def to_json(self):
+        return {self.id: {
+            "name": self.name
+        }}
+    
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+    
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+        
+    @classmethod
+    def get(cls, myId):
+        #Get always filters by primary_key
+        #None if id not found
+        return cls.query.filter_by(id=myId).first()
+    
+    @classmethod
+    def getByName(cls, myName):
+        return cls.query.filter_by(name=myName).all()
