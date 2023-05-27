422d740cd99d63a6854eb90176be7bced919d35f
AdiJoost <adi.joost@gmail.com>
2023-05-27 14:18:28 +0200

    bugfix

--- api/bfsApp.py
+++ api/bfsApp.py
@@ -21,6 +21,11 @@ def main():
             else:
                 order.changeStatus(SearchOrderModel.STATUS_PROCESSING)
 
+        #get with processing status
+        processingOrders = SearchOrderModel.getByStatus(SearchOrderModel.STATUS_SETUP)
+        for order in processingOrders:
+            processOrder(order)
+
 def insertOrder(order):
     nodes = getNodes(order.keyword)
     orderId = order.id
@@ -34,10 +39,18 @@ def insertOrder(order):
         for _id in idList:
             connectList.append(ConnectModel(id=_id,tablename=table, orderid=orderId))
     ConnectModel.multiInsert(connectList)
+    #if starting_node is already inserted, queue it
+    startNodeTableName, startNodeId = order.start_node.split(",")
+    startNode = ConnectModel.get(id=startNodeId, orderid=order.id, tablename=startNodeTableName)
+    if not startNode:
+        #insert stating_node
+        startNode = ConnectModel(id=startNodeId, orderid=order.id, tablename=startNodeTableName)
+        startNode.save()
+    #queue startnode.
+    startNode.setVisited()
+    startNode.setDistance(0)
+    startNode.queue()
     return 1
-
-    
-    
 
 def getNodes(keyword):
     response = requests.get(f"{URL}/api/keyword/{keyword}")
@@ -45,6 +58,9 @@ def getNodes(keyword):
         return None
     nodes = response.json()
     return nodes
+
+def processOrder(order):
+    pass
 
 
 if __name__ == "__main__":
